319ad930829a2aa3dc9c78f9cf6fac8b1ebe7d22
SatakeYusuke19920527 <sataaaaak@gmail.com>
2020-04-20 18:26:39 +0900

    lesson 14 fin

--- src/components/user/Users.js
+++ src/components/user/Users.js
@@ -1,32 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
 
-export class Users extends Component {
-	state = {
-		users: [
-			{
-				id: '1',
-				login: 'mojombo',
-				avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-				html_url: 'https://github.com/mojombo'
-			},
-			{
-				id: '2',
-				login: 'defunkt',
-				avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-				html_url: 'https://github.com/defunkt"'
-			},
-			{
-				id: '3',
-				login: 'pythiidd',
-				avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-				html_url: 'https://github.com/pjhyett'
-			}
-		]
-	};
-	render() {
-		return <div style={userStyle}>{this.state.users.map((user) => <UserItem key={user.id} user={user} />)}</div>;
-	}
+const Users = props => {
+    const { loading, users } = props;
+    
+    return (
+        <div style={userStyle}>
+            {loading ? <h1>Loading...</h1> : users.map((user) => <UserItem key={user.id} user={user} />)}
+        </div>
+    );
 }
 
 const userStyle = {
