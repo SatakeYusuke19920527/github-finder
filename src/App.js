77a9c79e5fbe5faa5ec85522f6ae7a034b71dbdc
SatakeYusuke19920527 <sataaaaak@gmail.com>
2020-04-20 12:50:52 +0900

    余計なファイルを削除

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		const name = 'John Doe';
+		const loading = false;
+		const showName = false;
+
+		return (
+			<div className="App">
+				{loading ? <h4>Loading...</h4> : <h1>Hello from React {showName ? name : null}</h1>}
+
+				<div>Hello React my name is satake Yusuke 祐亮 Yusuke</div>
+				<label htmlFor="name">name</label>
+			</div>
+		);
+	}
 }
 
 export default App;
