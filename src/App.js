a9e5e791f811afbde9d0c5a5bca8aacd6d397f21
SatakeYusuke19920527 <sataaaaak@gmail.com>
2020-04-20 15:57:53 +0900

    section 12 fin

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/user/Users';
 
 class App extends Component {
 	render() {
-		const name = 'John Doe';
-		const loading = false;
-		const showName = false;
-
 		return (
 			<div className="App">
-				{loading ? <h4>Loading...</h4> : <h1>Hello from React {showName ? name : null}</h1>}
-
-				<div>Hello React my name is satake Yusuke 祐亮 Yusuke</div>
-				<label htmlFor="name">name</label>
+				<Navbar />
+				<div className="container">
+					<Users />
+				</div>
 			</div>
 		);
 	}
